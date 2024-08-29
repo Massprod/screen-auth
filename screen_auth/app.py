e718eb04bd73f14f6e694f57fa0e242733e720c3
Massprod <piankovpe@gmail.com>
2024-08-29 14:35:40 +0500

    add creation of the `admin` account, on start of the app

--- screen_auth/app.py
+++ screen_auth/app.py
@@ -1,12 +1,16 @@
+import os
 from uuid import uuid4
 from loguru import logger
 from dotenv import load_dotenv
 from fastapi import FastAPI, Request
 from contextlib import asynccontextmanager
 from fastapi.middleware.cors import CORSMiddleware
-from database.collections.collections import create_basic_collections
 from database.mongo_connection import mongo_client
 from routers.users.router import router as registration
+from constants import DB_AUTH_NAME, CLN_USERS, ADMIN_ROLE
+from services.users_related import gather_correct_user_data
+from database.collections.collections import create_basic_collections
+from routers.users.crud import db_get_user_by_username_password, db_create_new_user
 
 
 load_dotenv('.env')
@@ -60,6 +64,31 @@ async def prepare_db():
     logger.info(
         'Finished creating the basic DB collections'
     )
+    logger.info('Checking for admin account')
+    admin_login = os.getenv('admin_login')
+    admin_password = os.getenv('admin_password')
+    admin_exist = await db_get_user_by_username_password(
+        admin_login, DB_AUTH_NAME, CLN_USERS, db,
+    )
+    if admin_exist:
+        logger.info("Admin account check complete. It's already exist")
+    else:
+        logger.warning(
+            "Admin account check complete. It's not present."
+            " Creating main Admin account."
+        )
+        admin_data = {
+            'username': admin_login,
+            'userRole': ADMIN_ROLE,
+            'password': admin_password,
+        }
+        cor_admin_data = await gather_correct_user_data(admin_data)
+        await db_create_new_user(
+            cor_admin_data, DB_AUTH_NAME, CLN_USERS, db
+        )
+        logger.info(
+            'New main Admin account created.'
+        )
 
 
 @app.middleware('http')
