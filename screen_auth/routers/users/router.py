5787ec72e7d924f132dec8575b6ae4fdcabe9b6e
Massprod <piankovpe@gmail.com>
2024-08-29 14:35:06 +0500

    add endpoint for getting JWT token with correct credentials == `login`

--- screen_auth/routers/users/router.py
+++ screen_auth/routers/users/router.py
@@ -1,13 +1,18 @@
 from loguru import logger
 from fastapi.responses import JSONResponse
-from constants import DB_AUTH_NAME, CLN_USERS
 from motor.motor_asyncio import AsyncIOMotorClient
 from database.mongo_connection import mongo_client
 from services.jwt_service import create_access_token
-from routers.users.models.models import UserCreate, Token
-from fastapi import APIRouter, Body, Depends, HTTPException, status
-from routers.users.crud import db_get_user_by_username, db_create_a_new_user
-from services.users_related import fill_create_user_data, gather_token_response
+from routers.users.models.models import UserCreate, Token, UsernameStr, PasswordStr
+from fastapi import APIRouter, Body, Depends, HTTPException, status, Query
+from constants import DB_AUTH_NAME, CLN_USERS, ACCESS_TOKEN_EXPIRE_MINUTES
+from routers.users.crud import db_get_user_by_username_password, db_create_new_user
+from services.users_related import (
+    gather_correct_user_data,
+    gather_token_response,
+)
+from services.pass_service import verify_password
+
 
 router: APIRouter = APIRouter()
 
@@ -24,7 +29,7 @@ async def post_route_register_user(
         db: AsyncIOMotorClient = Depends(mongo_client.depend_client),
 ):
     user_data = user_data.model_dump()
-    exists = await db_get_user_by_username(
+    exists = await db_get_user_by_username_password(
         user_data['username'], DB_AUTH_NAME, CLN_USERS, db
     )
     if exists:
@@ -35,15 +40,62 @@ async def post_route_register_user(
             detail='User with provided `username` already exists',
             status_code=status.HTTP_400_BAD_REQUEST,
         )
-    await fill_create_user_data(user_data)
-    await db_create_a_new_user(
-        user_data, DB_AUTH_NAME, CLN_USERS, db
+    correct_data = await gather_correct_user_data(user_data)
+    await db_create_new_user(
+        correct_data, DB_AUTH_NAME, CLN_USERS, db
     )
     token_data = {
         'sub': user_data['username'],
         'userRole': user_data['userRole'],
     }
-    user_access_token = create_access_token(token_data, 1)
+    user_access_token = create_access_token(token_data, ACCESS_TOKEN_EXPIRE_MINUTES)
+    return JSONResponse(
+        content=await gather_token_response(user_access_token),
+        status_code=status.HTTP_200_OK,
+    )
+
+
+@router.get(
+    path='/login',
+    name='Login User',
+    description='Validates credentials and issues JWT in response',
+    response_model=Token,
+)
+async def get_route_login_user(
+        username: UsernameStr = Query(...,
+                                      description='Required `username` credential',
+                                      example='JohnDoe',
+                                      ),
+        password: PasswordStr = Query(...,
+                                      description='Required `password` credential',
+                                      example='1Aa2345678!@',
+                                      ),
+        db: AsyncIOMotorClient = Depends(mongo_client.depend_client),
+):
+    exists = await db_get_user_by_username_password(
+        username, DB_AUTH_NAME, CLN_USERS, db,
+    )
+    if not exists:
+        logger.warning(
+            f'Attempt to login with incorrect credentials `username` = {username}'
+        )
+        raise HTTPException(
+            detail='Incorrect username',
+            status_code=status.HTTP_403_FORBIDDEN,
+        )
+    if not await verify_password(password, exists['hashedPassword']):
+        logger.warning(
+            f'Attempt to login with incorrect credentials `username` = {username} | `password` = {password}'
+        )
+        raise HTTPException(
+            detail='Incorrect password',
+            status_code=status.HTTP_403_FORBIDDEN,
+        )
+    token_data = {
+        'sub': exists['username'],
+        'userRole': exists['userRole'],
+    }
+    user_access_token = create_access_token(token_data, ACCESS_TOKEN_EXPIRE_MINUTES)
     return JSONResponse(
         content=await gather_token_response(user_access_token),
         status_code=status.HTTP_200_OK,
