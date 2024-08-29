044bcbdb320ac01559c6e726b5a1a2235aa7d6e0
Massprod <piankovpe@gmail.com>
2024-08-29 16:51:33 +0500

    add token verification and endpoint protection with OAuth2

--- screen_auth/services/jwt_service.py
+++ screen_auth/services/jwt_service.py
@@ -1,6 +1,15 @@
+from loguru import logger
 from jose import JWTError, jwt
+from fastapi.security import OAuth2PasswordBearer
 from datetime import datetime, timedelta, timezone
-from constants import PRIVATE_KEY, PUBLIC_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
+from fastapi import Depends, HTTPException, status
+from constants import (
+    PRIVATE_KEY,
+    PUBLIC_KEY,
+    ALGORITHM,
+    ACCESS_TOKEN_EXPIRE_MINUTES,
+    ADMIN_ROLE,
+)
 
 
 def create_access_token(data: dict, expiration_delta: int = ACCESS_TOKEN_EXPIRE_MINUTES) -> str:
@@ -21,3 +30,58 @@ def verify_token(token: str) -> dict | None:
         return payload
     except JWTError:
         return None
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='users/login')
+
+
+async def get_current_user_role(token: str = Depends(oauth2_scheme)) -> str:
+    logger.info(
+        f'Verifying access token'
+    )
+    try:
+        payload = verify_token(token)
+        role: str | None = payload.get('userRole')
+        if role is None:
+            logger.warning(
+                f'Attempt to use incorrect token = {token}'
+            )
+            raise HTTPException(
+                detail='Invalid token',
+                status_code=status.HTTP_401_UNAUTHORIZED,
+            )
+        return role
+    except JWTError as error:
+        logger.error(
+            f'Error while verifying provided token | Error: {error}'
+        )
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Invalid token'
+        )
+
+
+async def verify_admin_token(token: str = Depends(oauth2_scheme)) -> str:
+    logger.info(
+        f'Verifying ADMIN access token'
+    )
+    try:
+        payload = verify_token(token)
+        role: str | None = payload.get('userRole')
+        if ADMIN_ROLE != role:
+            logger.warning(
+                f'Attempt to use incorrect ADMIN token = {token}'
+            )
+            raise HTTPException(
+                detail='Invalid token',
+                status_code=status.HTTP_401_UNAUTHORIZED,
+            )
+        return payload.get('sub')
+    except JWTError as error:
+        logger.error(
+            f'Error while verifying provided token | Error: {error}'
+        )
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Invalid token'
+        )
