8b640d9e4633b1ceb3a5e9fdfc6c7ccc8af04e32
Massprod <piankovpe@gmail.com>
2024-09-02 10:46:31 +0500

    change expiration delta to seconds (from minutes) | add token verification for a manager role

--- screen_auth/services/jwt_service.py
+++ screen_auth/services/jwt_service.py
@@ -7,14 +7,15 @@ from constants import (
     PRIVATE_KEY,
     PUBLIC_KEY,
     ALGORITHM,
-    ACCESS_TOKEN_EXPIRE_MINUTES,
+    ACCESS_TOKEN_EXPIRE_SECONDS,
     ADMIN_ROLE,
+    MANAGER_ROLE,
 )
 
 
-def create_access_token(data: dict, expiration_delta: int = ACCESS_TOKEN_EXPIRE_MINUTES) -> str:
+def create_access_token(data: dict, expiration_delta: int = ACCESS_TOKEN_EXPIRE_SECONDS) -> str:
     to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + timedelta(minutes=expiration_delta)
+    expire = datetime.now(timezone.utc) + timedelta(seconds=expiration_delta)
     to_encode.update(
         {
             "exp": expire,
@@ -28,8 +29,14 @@ def verify_token(token: str) -> dict | None:
     try:
         payload = jwt.decode(token, PUBLIC_KEY, algorithms=[ALGORITHM])
         return payload
-    except JWTError:
-        return None
+    except JWTError as error:
+        logger.error(
+            f'Error while verifying provided token | Error: {error}'
+        )
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Invalid token'
+        )
 
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='users/login')
@@ -73,7 +80,7 @@ async def verify_admin_token(token: str = Depends(oauth2_scheme)) -> str:
                 f'Attempt to use incorrect ADMIN token = {token}'
             )
             raise HTTPException(
-                detail='Invalid token',
+                detail='Invalid Token',
                 status_code=status.HTTP_401_UNAUTHORIZED,
             )
         return payload.get('sub')
@@ -83,5 +90,31 @@ async def verify_admin_token(token: str = Depends(oauth2_scheme)) -> str:
         )
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail='Invalid token'
+            detail='Invalid Token'
+        )
+
+
+async def verify_manager_token(token: str = Depends(oauth2_scheme)) -> str:
+    logger.info(
+        f'Verifying {MANAGER_ROLE.upper()} access token'
+    )
+    try:
+        payload = verify_token(token)
+        role: str | None = payload.get('userRole')
+        if MANAGER_ROLE != role and ADMIN_ROLE != role:
+            logger.warning(
+                f'Attempt to use incorrect MANAGER|ADMIN token = {token}'
+            )
+            raise HTTPException(
+                detail='Invalid Token',
+                status_code=status.HTTP_401_UNAUTHORIZED,
+            )
+        return payload.get('sub')
+    except JWTError as error:
+        logger.error(
+            f'Error while verifying provided token | Error: {error}'
+        )
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Invalid Token',
         )
