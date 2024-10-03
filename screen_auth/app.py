c1a1784951cd5b54c3f4240d8d00bd933ac60ce8
Massprod <piankovpe@gmail.com>
2024-10-03 14:24:38 +0500

    add creation of basic accounts

--- screen_auth/app.py
+++ screen_auth/app.py
@@ -7,17 +7,19 @@ from contextlib import asynccontextmanager
 from fastapi.middleware.cors import CORSMiddleware
 from database.mongo_connection import mongo_client
 from routers.users.router import router as registration
-from constants import DB_AUTH_NAME, CLN_USERS, ADMIN_ROLE
 from services.users_related import gather_correct_user_data
 from database.collections.collections import create_basic_collections
+from constants import DB_AUTH_NAME, CLN_USERS, ADMIN_ROLE, CELERY_WORKER
 from routers.users.crud import db_get_user_by_username, db_create_new_user
 
 
 load_dotenv('.env')
 
+log_dir = 'logs/'
+os.makedirs(log_dir, exist_ok=True)
 
 logger.add(
-    'logs/logs/log',
+    os.path.join(log_dir, 'log.log'),
     rotation='50mb',
     retention='14 days',
     compression='zip',
@@ -38,8 +40,6 @@ app: FastAPI = FastAPI(
     version='0.0.1',
     description='Basic JWT authentication service',
     lifespan=lifespan,
-    # TODO: remove debug, after completion.
-    debug=True,
 )
 
 
@@ -64,9 +64,10 @@ async def prepare_db():
     logger.info(
         'Finished creating the basic DB collections'
     )
-    logger.info('Checking for admin account')
-    admin_login = os.getenv('admin_login')
-    admin_password = os.getenv('admin_password')
+    # + FIRST ADMIN ACCOUNT +
+    logger.info('Checking for `admin` account')
+    admin_login: str = os.getenv('API_F_ADMIN_LOGIN')
+    admin_password: str = os.getenv('API_F_ADMIN_PWD')
     admin_exist = await db_get_user_by_username(
         admin_login, DB_AUTH_NAME, CLN_USERS, db,
     )
@@ -89,6 +90,34 @@ async def prepare_db():
         logger.info(
             'New main Admin account created.'
         )
+    # - FIRST ADMIN ACCOUNT -
+    # + FIRST CELERY WORKER +
+    logger.info('Checking for basic `celeryWorker` account')
+    celery_login: str = os.getenv('CELERY_F_WORKER')
+    celery_password: str = os.getenv('CELERY_F_WORKER_PWD')
+    first_celery_exist = await db_get_user_by_username(
+        celery_login, DB_AUTH_NAME, CLN_USERS, db,
+    )
+    if first_celery_exist:
+        logger.info('Basic `celeryWorker` already exists.')
+    else:
+        logger.warning(
+            "Basic 'celeryWorker' account check complete. It's not present."
+            " Creating basic `celeryWorker` account."
+        )
+        celery_worker_data = {
+            'username': celery_login,
+            'userRole': CELERY_WORKER,
+            'password': celery_password,
+        }
+        cor_celery_worker_data = await gather_correct_user_data(celery_worker_data)
+        await db_create_new_user(
+            cor_celery_worker_data, DB_AUTH_NAME, CLN_USERS, db
+        )
+        logger.info(
+            "New basic `celeryWorker` account created."
+        )
+    # - FIRST CELERY WORKER -
 
 
 @app.middleware('http')
--- screen_auth/constants.py
+++ screen_auth/constants.py
@@ -4,7 +4,7 @@ from os import getenv
 
 FLD_BASIC_SCHEMAS: str = 'database/collections/schemas'
 # DB
-DB_AUTH_NAME: str = 'screenAuth'
+DB_AUTH_NAME: str = getenv('API_MONGO_DB_NAME', 'screenAuth')
 # CLN
 CLN_USERS: str = 'users'
 # ROLES
@@ -12,6 +12,8 @@ MANAGER_ROLE: str = 'manager'
 LAB_PERSONAL_ROLE: str = 'labPersonal'
 OPERATOR_ROLE: str = 'operator'
 ADMIN_ROLE: str = 'admin'
+CELERY_WORKER: str = 'celeryWorker'
+SYSTEM_ROLES: set[str] = {CELERY_WORKER}
 
 # JWT_token config
 PRIVATE_KEY_PATH = Path('certificates/private_key.pem')
@@ -27,7 +29,7 @@ ALGORITHM = getenv('jwt_algorithm')
 ACCESS_TOKEN_EXPIRE_SECONDS = int(getenv('jwt_expiration'))
 # ---
 # RESET PASS
-RESET_PASSWORD = getenv('reset_password')
+RESET_PASSWORD = getenv('STANDARD_RESET_PWD')
 
 
 # User Example
